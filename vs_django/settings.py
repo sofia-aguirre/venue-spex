3166d6cbd252bb32cf785a2480229a595a2c5d8c
sofia-aguirre <sofiaxaguirre@gmail.com>
2018-10-17 12:52:55 -0700

    fixed secret key

--- vs_django/settings.py
+++ vs_django/settings.py
@@ -24,11 +24,10 @@ MEDIA_DIR = os.path.join(BASE_DIR,'vs_app/media')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = 'aowbowgb2038r8248429urjfdFA#@V%WU^B%^@V%#!ECFV%B^$V%#C'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
